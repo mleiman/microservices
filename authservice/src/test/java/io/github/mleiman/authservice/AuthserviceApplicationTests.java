51c4263ea3da96248089891bcc88191737820775
mleiman <marcin.leiman@gmail.com>
2025-04-02 00:07:01 +0100

    project init

---
+++ authservice/src/test/java/io/github/mleiman/authservice/AuthserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mleiman.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
